276b464d1d50a06cc319ead494dde6aaef3cc074
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-13 08:52:18 +0100

    Flutter Logo

--- lib/screens/home_screen_rlg.dart
+++ lib/screens/home_screen_rlg.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const FlutterLogo(size: 150),
+          const SizedBox(height: 60),
+          Center(
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text("Sign in"))),
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: const Text("Sign up")),
+        ],
       ),
     );
   }
